26bc64d2c030048070fe1e53154dcc9ef6455dc2
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-05-28 10:54:00 +0600

    fixed the method of apply for accountOpened event in AccountAggregate class.

--- bank_account/bank_account/account.cmd/src/main/java/com/arka99/account/cmd/domain/AccountAggregate.java
+++ bank_account/bank_account/account.cmd/src/main/java/com/arka99/account/cmd/domain/AccountAggregate.java
@@ -28,7 +28,7 @@ public class AccountAggregate extends AggregateRoot {
     public void apply(AccountOpenedEvent event) {
         this.id = event.getId();
         this.active = true;
-        this.balance = event.getOpeningBalance();
+        this.balance += event.getOpeningBalance();
     }
 
     public void depositFunds(double amount) {
