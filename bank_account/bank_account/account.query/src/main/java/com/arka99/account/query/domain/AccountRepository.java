76e5fe3b7193988c975b704ef9100c3418a457f5
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-05-28 15:22:25 +0600

    Added the repository interface for BankAccount class.

---
+++ bank_account/bank_account/account.query/src/main/java/com/arka99/account/query/domain/AccountRepository.java
@@ -0,0 +1,15 @@
+package com.arka99.account.query.domain;
+
+import com.arka99.cqrs.core.domain.BaseEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends CrudRepository<BankAccount, String> {
+    Optional<BankAccount> findByAccountHolder(String accountHolder);
+
+    List<BaseEntity> findByBalanceGreaterThan(double balance);
+
+    List<BaseEntity> findByBalanceLessThan(double balace);
+}
