da2fbac7402ceabdaf494b72607826fdcdb9f725
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-05-28 13:11:14 +0600

    registered the command handlers in the command application file.

--- bank_account/bank_account/account.cmd/src/main/java/com/arka99/account/cmd/CommandApplication.java
+++ bank_account/bank_account/account.cmd/src/main/java/com/arka99/account/cmd/CommandApplication.java
@@ -1,13 +1,31 @@
 package com.arka99.account.cmd;
 
+import com.arka99.account.cmd.api.command.*;
+import com.arka99.cqrs.core.infrastructure.CommandDispatcher;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommandApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommandApplication.class, args);
-	}
+    @Autowired
+    private CommandDispatcher commandDispatcher;
+
+    @Autowired
+    private CommandHandler commandHandler;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommandApplication.class, args);
+    }
+
+    @PostConstruct
+    public void registerHandlers() {
+        commandDispatcher.registerHandler(OpenAccountCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(DepositFundsCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(WithdrawFundsCommand.class, commandHandler::handle);
+        commandDispatcher.registerHandler(CloseAccountCommand.class, commandHandler::handle);
+    }
 
 }
