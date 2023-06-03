8b8abb8bfa4d218ee183d7a0561632e41dbc8346
Arka Bhuiyan <arka.bhuiyan@technonext.com>
2023-06-03 17:58:21 +0600

    Added the EventConsumer interface for read databaase.

---
+++ bank_account/bank_account/account.query/src/main/java/com/arka99/account/query/infrastructure/consumers/EventConsumer.java
@@ -0,0 +1,18 @@
+package com.arka99.account.query.infrastructure.consumers;
+
+import com.arka99.account.common.events.AccountClosedEvent;
+import com.arka99.account.common.events.AccountOpenedEvent;
+import com.arka99.account.common.events.FundsDepositedEvent;
+import com.arka99.account.common.events.FundsWithdrawnEvent;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
+
+public interface EventConsumer {
+    void consume(@Payload AccountOpenedEvent openedEvent, Acknowledgment acknowledgment);
+
+    void consume(@Payload FundsDepositedEvent depositedEvent, Acknowledgment acknowledgment);
+
+    void consume(@Payload FundsWithdrawnEvent fundsWithdrawnEvent, Acknowledgment acknowledgment);
+
+    void consume(@Payload AccountClosedEvent closedEvent, Acknowledgment acknowledgment);
+}
